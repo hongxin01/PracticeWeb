3df315d2068bd9d540e0bab9c33de88c76e3a386
hongxin.shao <hongxin.shao@gmail.com>
2017-09-27 02:57:27 +0800

    json to springboot
    json to springboot

--- demo/src/main/java/com/example/demo/helloController.java
+++ demo/src/main/java/com/example/demo/helloController.java
@@ -8,6 +8,9 @@ public class helloController {
     @RequestMapping("/sys")
     public String index(){
         System.out.println("enter controller...");
-        return "hi:hongxin.shao,welcome to spring boot!";
+        String ss = "hongxin";
+        String mingming = "pengfei";
+        String str = ss+" he " +mingming;
+        return str+"hi:hongxin.shao,welcome to spring boot!";
     }
 }
