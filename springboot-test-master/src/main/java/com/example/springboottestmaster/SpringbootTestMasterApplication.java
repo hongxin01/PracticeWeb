e52b7c5643a4617828f05be5690c4c086962180c
hongxin.shao <hongxin.shao@gmail.com>
2017-09-27 03:36:44 +0800

    s s s
    sss

---
+++ springboot-test-master/src/main/java/com/example/springboottestmaster/SpringbootTestMasterApplication.java
@@ -0,0 +1,12 @@
+package com.example.springboottestmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTestMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTestMasterApplication.class, args);
+	}
+}
